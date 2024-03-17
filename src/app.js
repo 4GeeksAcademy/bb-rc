06b0d4de21aa19e1ea8a6024f9e380c9f840efdb
Brandon Berrios <140827644+brandonberrios@users.noreply.github.com>
2024-03-17 04:54:59 +0000

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
 /* eslint-disable */
-import "bootstrap";
+/*import "bootstrap";*/
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNum();
+};
+
+let button = document.getElementById("button");
+button.addEventListener("click", () => {
+  let card = document.querySelector(".card");
+  let removeClassName = card.classList["1"];
+  //remove suit/class from onload
+  document.querySelector(".card").classList.remove(removeClassName);
+  //add new random suit/class
+  card.classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNum();
+});
+let generateRandomNum = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
